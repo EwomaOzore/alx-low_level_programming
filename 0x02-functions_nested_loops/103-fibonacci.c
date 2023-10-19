da3bee7f0922e8aa8599d3a01a241e62c3710a64
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-19 14:46:17 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long int first = 1, second = 2, next, sum = 0;
+
+    while (second <= 4000000)
+    {
+        if (second % 2 == 0)
+            sum += second;
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    printf("%ld\n", sum);
+
+    return (0);
+}
