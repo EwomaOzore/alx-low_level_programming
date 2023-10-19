19b46a0c9d5013a6b1ed4801153dfb13d978f9a9
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-19 09:34:50 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, last_digit;
+
+	srand(time(0)); /* Seed the random number generator with the current time */
+	n = rand() - RAND_MAX / 2; /* Generate a random number and store it in 'n' */
+
+	last_digit = n % 10; /* Calculate the last digit of 'n' */
+
+	printf("Last digit of %d is %d and is ", n, last_digit);
+
+	if (last_digit > 5)
+	{
+		printf("greater than 5\n");
+	}
+	else if (last_digit == 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("less than 6 and not 0\n");
+	}
+
+	return (0);
+}
