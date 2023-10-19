e776201b8509cec1e9c3d1edbe30f33990bdabe4
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-19 15:09:00 -0700

    initial commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,12 +5,12 @@ int main(void) {
     int count = 0;
 
     printf("%lu, %lu, ", a, b);
-    count = 2;  // Start with 2 numbers already printed.
+    count = 2;  /* Start with 2 numbers already printed. */
 
     while (count < 98) {
         c = a + b;
         if (count == 97) {
-            printf("%lu\n", c);  // Last number, so print with a newline.
+            printf("%lu\n", c);  /* Last number, so print with a newline. */
         } else {
             printf("%lu, ", c);
         }
