63b9d9707e33a7ec9c23e9bb57ba45b3101b0c63
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-19 11:06:05 -0700

    initial commit

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writes a character to the standard output (stdout)
  * @c: The character to print
  *
  * Return: On success 1.
