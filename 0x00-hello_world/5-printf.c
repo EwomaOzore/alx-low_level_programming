3d79ad7eb4c172fddddec97a67afa263ae53ad2f
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-19 06:51:56 -0700

    initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- * within 80 characters.
+ * Description: Prints a specific text with proper grammar.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
