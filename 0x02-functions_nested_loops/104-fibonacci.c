88f74c012ebaa17659fdbe6e86ffb3c5aa8b3998
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-19 15:06:59 -0700

    initial commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
-/**
- * main - Entry point.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    unsigned long a = 1, b = 2, c, count = 0;
+int main(void) {
+    unsigned long int a = 1, b = 2, c;
+    int count = 0;
 
     printf("%lu, %lu, ", a, b);
-    count = 2;  /* Start with 2 numbers already printed. */
+    count = 2;  // Start with 2 numbers already printed.
 
-    while (count < 98)
-    {
+    while (count < 98) {
         c = a + b;
-        if (count == 97)
-        {
-            /* Print the last number without a comma. */
-            printf("%lu\n", c);
-        }
-        else
-        {
+        if (count == 97) {
+            printf("%lu\n", c);  // Last number, so print with a newline.
+        } else {
             printf("%lu, ", c);
         }
         a = b;
