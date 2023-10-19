db8bb4529eddcfaaaf21d9e0125a80739ecb322a
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-19 13:04:38 -0700

    initial commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,18 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
 
-    if (n < 0)
-        last_digit = -n % 10;
-    else
-        last_digit = n % 10;
+	if (n == 0)
+		last_digit = 0;
+	else if (n == INT_MIN)  /* Handle the special case of INT_MIN */
+		last_digit = 8;      /* The last digit of INT_MIN is 8 */
+	else if (n < 0)
+		last_digit = -n % 10;
+	else
+		last_digit = n % 10;
 
-    _putchar('0' + last_digit);
+	_putchar('0' + last_digit);
 
-    return last_digit;
+	return last_digit;
 }
