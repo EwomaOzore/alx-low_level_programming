d1ebf6fd893fb23de5e45d03dadff60996d8fd1a
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-20 01:07:08 -0700

    initial commit

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
     char c;
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototype for _isupper function */
 int _isupper(int c);
 
-#endif
+#endif /* MAIN_H */
