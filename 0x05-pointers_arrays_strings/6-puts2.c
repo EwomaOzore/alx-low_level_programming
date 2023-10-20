008416be5415141360085db997c1afa3b048ef84
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-20 12:56:33 -0700

    initial commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,15 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-        if (str[i] == '\0')
-            break;
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+		if (str[i] == '\0')
+			break;
+		i++;
+	}
+	_putchar('\n');
 }
