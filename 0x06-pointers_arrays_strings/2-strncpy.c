58050e0511eeed3bbaa13eb4a2268066e3234425
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-20 15:31:48 -0700

    initial commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,22 +10,22 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    char *ptr = dest;
+	char *ptr = dest;
 
-    while (*src && n > 0)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;
-    }
+	while (*src && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
 
-    while (n > 0)
-    {
-        *dest = '\0';
-        dest++;
-        n--;
-    }
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
 
-    return ptr;
+	return (ptr);
 }
