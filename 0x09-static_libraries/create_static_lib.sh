cd002008f1973a1da5dae53d612b25c35ab1e059
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-27 14:39:17 -0700

    initial commit

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Compile each .c file into a corresponding .o file
+gcc -c *.c
+
+# Create the static library liball.a from all the .o files
+ar rc liball.a *.o
+
+# Index the static library
+ranlib liball.a
+
+# Clean up the .o files
+rm -f *.o
