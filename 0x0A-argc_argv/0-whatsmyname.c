de962cbb9d8a4be50fd3e49b2ce7d005294ea7a9
Ewoma Ozore <ewomaozore@gmail.com>
2023-10-27 15:22:56 -0700

    initial commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
