996d36abfaf8dc927efcef6e956cbdd3c5ecfc8d
Ewoma Ozore <ewomaozore@gmail.com>
2023-11-06 10:32:33 -0800

    initial commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints the name of the source file.
+ *
+ * Return: Always 0 (success).
+ */
 int main(void)
 {
 	printf(__FILE__ "\n");
