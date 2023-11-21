d225048b05fcfc1023197dc126c2e2433046a744
Ewoma Ozore <ewomaozore@example.com>
2023-11-20 23:58:13 -0800

    initial commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
 /**
  * pop_listint - Deletes the head node of a listint_t linked list.
  * @head: Pointer to the pointer of the head of the list.
- * 
+ *
  * Return: The head node’s data (n), or 0 if the linked list is empty.
  */
 int pop_listint(listint_t **head)
