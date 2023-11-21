fd5ef6327674982d68c6b88df8fc3277b805fbdd
Ewoma Ozore <ewomaozore@example.com>
2023-11-21 00:53:52 -0800

    initial commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * print_listint_safe - Prints a listint_t linked list safely.
  * @head: Pointer to the head of the list.
+ *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
@@ -14,7 +15,7 @@ size_t print_listint_safe(const listint_t *head)
 
 	tortoise = hare = head;
 
-	while (hare != NULL && hare->next != NULL)
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
 	{
 		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		tortoise = tortoise->next;
@@ -24,6 +25,20 @@ size_t print_listint_safe(const listint_t *head)
 
 		if (tortoise == hare)
 		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+				tortoise = tortoise->next;
+				hare = hare->next;
+
+				if (hare == NULL || hare->next == NULL)
+				{
+					printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+					return (count);
+				}
+			}
+
 			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
 			return (count);
 		}
