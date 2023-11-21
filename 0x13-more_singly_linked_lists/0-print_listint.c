bdc5f71a61cb8d4f8d16d36e805b69ffd5a34b1b
Ewoma Ozore <ewomaozore@example.com>
2023-11-20 23:30:04 -0800

    initial commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: Pointer to the head of the list.
- * 
- * Return: The number of nodes.
- */
+* print_listint - Prints all the elements of a listint_t list.
+* @h: Pointer to the head of the list.
+*
+* Return: The number of nodes.
+*/
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
